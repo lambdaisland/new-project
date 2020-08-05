7b162ce4c38a31240dc376741f9255b43f6976a1
Arne Brasseur <arne@arnebrasseur.net>
2020-08-05 10:57:58 +0200

    Revert "Remove ClojureScript setup"
    This reverts commit 8f893b40e5cb4edb8f004a1d2433b8b592e93933.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
+         (cider-default-cljs-repl              . browser)
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
          (cider-repl-display-help-banner       . nil)
