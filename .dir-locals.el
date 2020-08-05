8f893b40e5cb4edb8f004a1d2433b8b592e93933
Arne Brasseur <arne@arnebrasseur.net>
2020-08-05 10:57:34 +0200

    Remove ClojureScript setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
-         (cider-default-cljs-repl              . browser)
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
          (cider-repl-display-help-banner       . nil)
