1864215d9d4238ed36914d70794fd3d85c1112cc
Arne Brasseur <arne@arnebrasseur.net>
2021-08-13 08:48:20 +0200

    Start a branch for lambdaisland libs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,1 @@
-((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
-         (cider-preferred-build-tool           . clojure-cli)
-         (cider-redirect-server-output-to-repl . t)
-         (cider-repl-display-help-banner       . nil)
-         (clojure-toplevel-inside-comment-form . t))))
+((nil . ((cider-clojure-cli-global-options     . "-A:dev:test"))))
