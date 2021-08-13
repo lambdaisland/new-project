4b0c1c0426a61da179c7b37d54e0dbe779a8d840
Arne Brasseur <arne@arnebrasseur.net>
2021-08-13 08:40:42 +0200

    sync

--- src/project/http.clj
+++ src/project/http.clj
@@ -5,7 +5,7 @@
             [reitit.interceptor.sieppari :as sieppari]
             [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
-            [{{project.routes}} :as routes]))
+            [{{project}}.routes :as routes]))
 
 (defn interceptor [number]
   {:enter (fn [ctx] (update-in ctx [:request :number] (fnil + 0) number))})
@@ -17,7 +17,7 @@
    {:executor sieppari/executor
     :interceptors [(muuntaja.interceptor/format-interceptor)]}))
 
-(defmethod ig/init-key ::server [{:keys [port rebuild-on-request?]}]
+(defmethod ig/init-key ::server [_ {:keys [port rebuild-on-request?]}]
   (jetty/run-jetty (if rebuild-on-request?
                      #((handler) %)
                      (handler))
